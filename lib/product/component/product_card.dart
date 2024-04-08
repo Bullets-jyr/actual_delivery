e0093d7a7c0feb513667eed9cb133a9b31b87b65
Bullets-jyr <jyr@bullets.co.kr>
2024-04-08 13:43:39 +0900

    ProductCard 매핑하기

--- lib/product/component/product_card.dart
+++ lib/product/component/product_card.dart
@@ -1,8 +1,37 @@
 import 'package:actual_delivery/common/const/colors.dart';
 import 'package:flutter/material.dart';
 
+import '../../restaurant/model/restaurant_detail_model.dart';
+
 class ProductCard extends StatelessWidget {
-  const ProductCard({super.key});
+  final Image image;
+  final String name;
+  final String detail;
+  final int price;
+
+  const ProductCard({
+    required this.image,
+    required this.name,
+    required this.detail,
+    required this.price,
+    super.key,
+  });
+
+  factory ProductCard.fromModel({
+    required RestaurantProductModel model,
+  }) {
+    return ProductCard(
+      image: Image.network(
+        model.imgUrl,
+        width: 110,
+        height: 110,
+        fit: BoxFit.cover,
+      ),
+      name: model.name,
+      detail: model.detail,
+      price: model.price,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +40,13 @@ class ProductCard extends StatelessWidget {
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(8.0),
-            child: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              width: 110,
-              height: 110,
-              fit: BoxFit.cover,
-            ),
+            child: image,
+            // child: Image.asset(
+            //   'asset/img/food/ddeok_bok_gi.jpg',
+            //   width: 110,
+            //   height: 110,
+            //   fit: BoxFit.cover,
+            // ),
           ),
           const SizedBox(width: 16.0),
           Expanded(
@@ -25,14 +55,14 @@ class ProductCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  '떡볶이',
+                  name,
                   style: TextStyle(
                     fontSize: 18.0,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
                 Text(
-                  '전통 떡볶이의 정석! 원하는대로 맵기를 선택하고 추억의 떡볶이맛에 빠져보세요! 쫀득한 쌀떡과 말랑한 오뎅의 완벽한 조화! 잘익은 반숙 계란은 덤!',
+                  detail,
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                   style: TextStyle(
@@ -41,7 +71,7 @@ class ProductCard extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  '₩10000',
+                  '₩$price',
                   textAlign: TextAlign.right,
                   style: TextStyle(
                     color: PRIMARY_COLOR,
