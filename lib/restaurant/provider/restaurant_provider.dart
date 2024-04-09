347a5773e0373be00208d5655ee0a5a7ec161b98
Bullets-jyr <jyr@bullets.co.kr>
2024-04-09 19:27:56 +0900

    Restaurant Pagination - 2

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -24,7 +24,7 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationModelBase> {
     paginate();
   }
 
-  paginate({
+  void paginate({
     int fetchCount = 20,
     // 추가로 데이터 더 가져오기
     // true - 추가로 데이터 더 가져옴
@@ -41,6 +41,19 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationModelBase> {
     // 2) CursorPaginationModelLoading - 데이터가 로딩 중인 상태 (현재 캐시 없음), forceRefetch가 true인 경우
     // 3) CursorPaginationModelError - 에러가 있는 상태
     // 4) CursorPaginationModelRefetching - 첫 번째 페이지부터 다시 데이터를 가져올 때
-    // 4) CursorPaginationModelFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을 때
+    // 5) CursorPaginationModelFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을 때
+
+    // 바로 반환(return)하는 상황
+    // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고있다면)
+    // 2) 로딩 중 - fetchMore가 true일 때
+    // - 로딩 중일 때 반환(return)을 안해주는 상황은? fetchMore가 false일 때, 새로고침의 의도가 있다
+
+    if (state is CursorPaginationModel && !forceRefetch) {
+      final pState = state as CursorPaginationModel;
+
+      if (!pState.meta.hasMore) {
+        return;
+      }
+    }
   }
 }
