dbb16398d4a552427bccc8c2c73c9fd803b02761
Bullets-jyr <jyr@bullets.co.kr>
2024-04-11 20:50:48 +0900

    UserMeRepository 생성하기

---
+++ lib/user/repository/user_me_repository.dart
@@ -0,0 +1,27 @@
+import 'package:actual_delivery/common/dio/dio.dart';
+import 'package:actual_delivery/user/model/user_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/http.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../common/const/data.dart';
+
+part 'user_me_repository.g.dart';
+
+final userMeRepositoryProvider = Provider<UserMeRepository>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+
+    return UserMeRepository(dio, baseUrl: 'http://$ip/user/me');
+  },
+);
+
+// http://$ip/user/me
+@RestApi()
+abstract class UserMeRepository {
+  factory UserMeRepository(Dio dio, {String baseUrl}) = _UserMeRepository;
+
+  @GET('/')
+  @Headers({'accessToken': 'true'})
+  Future<UserModel> getMe();
+}
